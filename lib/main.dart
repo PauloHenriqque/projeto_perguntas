1b75cacf1c834e0cb1012e136631ff8ff46dcc81
Paulo Henrique <148291338+PauloHenriqque@users.noreply.github.com>
2025-03-27 14:59:30 -0300

    Refactor quiz logic and enhance question handling in PerguntaApp

--- lib/main.dart
+++ lib/main.dart
@@ -7,43 +7,69 @@ main() {
 }
 
 class _PerguntaAppState extends State<PerguntaApp> {
-
   var _perguntaSelecionada = 0;
 
+  final _perguntas = const [
+    {
+      'texto': 'Qual é a sua cor favorita?',
+      'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
+    },
+    {
+      'texto': 'Qual é o seu animal favorito?',
+      'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
+    },
+    {
+      'texto': 'Qual é o seu instrutor favorito?',
+      'respostas': ['Maria', 'João', 'Leo', 'Pedro'],
+    },
+  ];
+
   void _responder() {
-    setState(() {
-      _perguntaSelecionada++;
-    });
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+      });
+    }
+  }
+
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
   }
 
   @override
   Widget build(BuildContext context) {
+    List<String> respostas =
+        temPerguntaSelecionada
+            ? _perguntas[_perguntaSelecionada].cast()['respostas']
+            : [];
 
-  final perguntas = [
-    'Qual é a sua cor favorita?',
-    'Qual é o seu animal favorito?'
-  ];
+    //for (String textoResp in respostas) {
+    //  widgets.add(Resposta(textoResp, _responder));
+    //}
+
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Perguntas'),
-        ),
-        body: Column(
-          children: <Widget>[
-            Questao(perguntas[_perguntaSelecionada]),
-            Resposta('Resposta 1', _responder),
-            Resposta('Resposta 2', _responder),
-            Resposta('Resposta 3', _responder),
-          ],
-        ),
+        appBar: AppBar(title: Text('Perguntas')),
+        body:
+            temPerguntaSelecionada
+                ? Column(
+                  children: <Widget>[
+                    Questao(
+                      _perguntas[_perguntaSelecionada]['texto'].toString(),
+                    ),
+                    ...respostas.map((t) => Resposta(t, _responder)).toList(),
+                  ],
+                )
+                : Center(
+                  child: Text('Parabéns!', style: TextStyle(fontSize: 28)),
+                ),
       ),
     );
   }
-
 }
 
 class PerguntaApp extends StatefulWidget {
   _PerguntaAppState createState() {
     return _PerguntaAppState();
   }
-} 
+}
